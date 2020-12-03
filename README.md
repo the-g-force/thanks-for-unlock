45edcfcbd5af44d772dba532bb60723b6aa83b03
Paul Gestwicki <pvgestwicki@bsu.edu>
2020-12-03 14:41:04 -0500

    Add failure message

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Thanks-for-Unlock
 
 The interaction portion of a family thank-you note.
+
+## Third-Party Assets
+
+- [Nerko One](https://fonts.google.com/specimen/Nerko+One) font used under [OFL](project/assets/fonts/Nerko_One/OFL.txt)
--- project/src/MainScene.gd
+++ project/src/MainScene.gd
@@ -2,8 +2,12 @@ extends Control
 
 export var SOLUTION := "6543"
 
-onready var _code_edit := $VBoxContainer/HBoxContainer/CodeEdit
-
+onready var _code_edit := $CodeEntryBox/HBoxContainer/CodeEdit
+onready var _buttons = [
+	$CodeEntryBox/ButtonBox/SubmitButton,
+	$CodeEntryBox/ButtonBox/ClearButton
+]
+onready var _animation_player := $AnimationPlayer
 
 func _on_ClearButton_pressed():
 	_code_edit.text = ""
@@ -13,4 +17,15 @@ func _on_SubmitButton_pressed():
 	if _code_edit.text == SOLUTION:
 		print("Correct")
 	else:
-		print("Incorrect")
+		_animation_player.play("Incorrect")
+
+func _disable_input():
+	for element in _buttons:
+		var button := element as Button
+		button.disabled = true
+
+
+func _enable_input():
+	for element in _buttons:
+		var button := element as Button
+		button.disabled = false
